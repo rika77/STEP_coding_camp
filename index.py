aac6a1720e5a4c001304f8c4c87a40b0468ab898
RikaFujimaki <mahimahi.kametan@gmail.com>
2018-03-31 12:18:12 +0900

    init

---
+++ index.py
@@ -0,0 +1,12 @@
+import os
+Import wp
+
+try:
+    # 過去のindexは削除する
+    os.remove("data/index.db")
+except OSError:
+    pass
+collection = wp.WikipediaCollection("data/wp.db")
+index = wp.Index("data/index.db", collection)
+index.generate()
+
